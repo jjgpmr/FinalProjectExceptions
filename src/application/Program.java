1c634be5e080acf845e191ba16ec9ea05a3e433d
José Rosa <maiarosajose@gmail.com>
2022-11-10 10:44:24 +0000

    Tratamento de excepções

---
+++ src/application/Program.java
@@ -0,0 +1,42 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Account;
+import model.exceptions.DomainExceptions;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter account data");
+		System.out.print("Number: ");
+		int number = sc.nextInt();
+		System.out.print("Holder: ");
+		sc.next();
+		String holder = sc.nextLine();
+		System.out.print("Initial balance: ");
+		double balance = sc.nextDouble();
+		System.out.print("Withdraw limit: ");
+		double limit = sc.nextDouble();
+		
+		Account account = new Account(number, holder, balance, limit);
+		
+		System.out.println();
+		System.out.print("Enter amount for withdraw: ");
+		double amount = sc.nextDouble();
+		
+		try {
+			account.withdraw(amount);
+			System.out.printf("New balance: %.2f" , account.getBalance());
+		}
+		catch(DomainExceptions e){
+			System.out.println(e.getMessage());
+		}
+	}
+
+}
